891c2eed6afa77441097e0e1d6ec4b9d08c96b18
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2024-10-01 21:14:06 -0300

    Cree los dto de auto

---
+++ src/main/java/backend/car_rental/dto/car/CreateCarDto.java
@@ -0,0 +1,53 @@
+package backend.car_rental.dto.car;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CreateCarDto {
+
+    @NotBlank(message = "The plate cannot be empty")
+    private String plate; 
+    
+    @NotBlank(message = "The brand cannot be empty")
+    private String brand; 
+
+    @NotBlank(message = "The model cannot be empty")
+    private String model; 
+
+    @NotBlank(message = "The year cannot be empty")
+    @Min(2000)
+    private int year;
+
+    @NotBlank(message = "The color cannot be empty")
+    private String color;
+
+    @Builder.Default
+    private int passengersAmount = 5; 
+
+    private boolean manual; 
+
+    private int suitcasesAmount; 
+
+    @Min(1)
+    private double pricePerDay;
+
+    public CreateCarDto(String plate, String brand, String model, int year,
+            String color, int passengersAmount, boolean manual,
+            int suitcasesAmount, @Min(1) double pricePerDay) {
+        this.plate = plate;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.passengersAmount = passengersAmount;
+        this.manual = manual;
+        this.suitcasesAmount = suitcasesAmount;
+        this.pricePerDay = pricePerDay;
+    } 
+
+    
+}
---
+++ src/main/java/backend/car_rental/dto/car/ResponseCarDto.java
@@ -0,0 +1,31 @@
+package backend.car_rental.dto.car;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ResponseCarDto {
+    private Long id;
+
+    private String plate; 
+    
+    private String brand; 
+
+    private String model; 
+
+    private int year; 
+
+    private String color;
+
+    @Builder.Default
+    private int passengersAmount = 5; 
+
+    private boolean manual; 
+
+    private int suitcasesAmount; 
+
+    private double pricePerDay; 
+
+    private boolean hidden;
+}
