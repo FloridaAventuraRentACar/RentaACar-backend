86d22f8bcd9a6e191ebe0385418a8839b93d4e8b
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2025-01-24 18:02:33 -0700

    agregue notacion CrossOrigin en avaibility

--- src/main/java/backend/car_rental/controlers/RentalController.java
+++ src/main/java/backend/car_rental/controlers/RentalController.java
@@ -1,21 +1,16 @@
 package backend.car_rental.controlers;
 
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import backend.car_rental.dto.rental.CreateRentalDto;
-import backend.car_rental.entities.Rental;
 import backend.car_rental.services.rental.interfaces.ISaveRentalService;
 import jakarta.validation.Valid;
 
