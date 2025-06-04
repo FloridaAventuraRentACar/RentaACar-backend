418bdf83b19ccfaca839dd0ab7588615ea029d07
felipedelzoppo <felipedelzoppo@gmail.com>
2025-06-04 12:30:50 -0300

    Cree endpoint findCurrentRentals. #30

--- src/main/java/backend/car_rental/repositories/IRentalRepository.java
+++ src/main/java/backend/car_rental/repositories/IRentalRepository.java
@@ -1,13 +1,16 @@
 package backend.car_rental.repositories;
 
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import backend.car_rental.entities.Rental;
 
 public interface IRentalRepository extends CrudRepository<Rental,Long>{
 
-
-    // @Query(select r from )
-    // List<Rental> findByDates(LocalDateTime start, LocalDateTime end);
+    @Query("select r from Rental r where r.end > ?1")
+    List<Rental> findRentalsAfterDate(LocalDateTime endDate);
 }
