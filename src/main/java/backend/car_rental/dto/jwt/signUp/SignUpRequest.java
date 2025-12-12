ea9b11b0aa0f0ef65d81e3f7c312282da4c30b22
felipedelzoppo <felipedelzoppo@gmail.com>
2025-12-12 10:39:05 -0300

    fix: Contraseña minimo 6 caracteres

--- src/main/java/backend/car_rental/dto/jwt/signUp/SignUpRequest.java
+++ src/main/java/backend/car_rental/dto/jwt/signUp/SignUpRequest.java
@@ -1,8 +1,8 @@
 package backend.car_rental.dto.jwt.signUp;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,7 +17,7 @@ public class SignUpRequest {
     @Email
     private String email;
 
-    @Min(6)
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     @NotBlank
     private String password;
 }
