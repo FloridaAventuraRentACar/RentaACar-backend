2142254d289b5c149cedff7cc3937a040c70eaa9
felipedelzoppo <felipedelzoppo@gmail.com>
2026-04-24 14:44:19 -0300

    feat: Cambio de precio de viajar a Orlando

--- src/main/java/backend/car_rental/enums/TravelLocation.java
+++ src/main/java/backend/car_rental/enums/TravelLocation.java
@@ -3,7 +3,7 @@ package backend.car_rental.enums;
 //This are the possible travel locations if the client is traveling away from Miami
 public enum TravelLocation {
     KeyWest(20.70),
-    Orlando(32.00),
+    Orlando(38.00),
     WestPalmBeach(32.50),
     Daytona(30.00),
     ClearWater(24.70),
